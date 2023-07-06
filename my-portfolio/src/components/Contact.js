37d1e840666f4b1d213639d92dd780b4996a43ec
Ryan Golder <rgolder78@gmail.com>
2023-07-06 21:36:42 +1000

    Update various

--- my-portfolio/src/components/Contact.js
+++ my-portfolio/src/components/Contact.js
@@ -1,20 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Contact = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [error, setError] = useState('');
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleMessageChange = (event) => {
+    setMessage(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Validate form fields
+    if (!name || !email || !message) {
+      setError('All fields are required');
+      return;
+    }
+
+    // Validate email format
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      setError('Invalid email address');
+      return;
+    }
+
+    // Clear form fields and error message
+    setName('');
+    setEmail('');
+    setMessage('');
+    setError('');
+
+    console.log('Form submitted');
+  };
+
   return (
     <section id="contact" className="text-center">
       <h2>Contact</h2>
-      <div className="contact-links">
-        <a className="btn btn-primary" href="mailto:rgolder78@gmail.com" target="_blank" rel="noopener noreferrer">
-          Email: rgolder78@gmail.com
-        </a>
-        <button className="btn btn-primary" onClick={() => window.open("https://github.com/RyanGolder", "_blank")} target="_blank" rel="noopener noreferrer">
-          GitHub
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Name"
+            value={name}
+            onChange={handleNameChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Email"
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <textarea
+            className="form-control"
+            rows="5"
+            placeholder="Message"
+            value={message}
+            onChange={handleMessageChange}
+            required
+          ></textarea>
+        </div>
+        {error && <div className="text-danger">{error}</div>}
+        <button type="submit" className="btn btn-primary">
+          Submit
         </button>
-        <button className="btn btn-primary" onClick={() => window.open("https://www.linkedin.com/in/ryan-golder-00b867138/", "_blank")} target="_blank" rel="noopener noreferrer">
-          LinkedIn
-        </button>
-      </div>
+      </form>
     </section>
   );
 };
--- my-portfolio/src/components/Footer.js
+++ my-portfolio/src/components/Footer.js
@@ -5,10 +5,10 @@ const Footer = () => {
     <footer className="bg-light">
       <div className="container">
         <div className="footer-links text-center py-4">
-          <a className="btn btn-primary" href="https://github.com/RyanGolder" target="_blank" rel="noopener noreferrer">
+          <a className="btn btn-primary mx-2" href="https://github.com/RyanGolder" target="_blank" rel="noopener noreferrer">
             GitHub
           </a>
-          <a className="btn btn-primary" href="https://www.linkedin.com/in/ryan-golder-00b867138/" target="_blank" rel="noopener noreferrer">
+          <a className="btn btn-primary mx-2" href="https://www.linkedin.com/in/ryan-golder-00b867138/" target="_blank" rel="noopener noreferrer">
             LinkedIn
           </a>
         </div>
